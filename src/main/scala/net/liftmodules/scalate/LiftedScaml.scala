36af0cc934eece9333341b0b254f4146eac08f8f
Tony Kay <tony@teamunify.com>
2013-02-10 18:06:08 -0800

    added template caching

--- src/main/scala/net/liftmodules/scalate/LiftedScaml.scala
+++ src/main/scala/net/liftmodules/scalate/LiftedScaml.scala
@@ -1,6 +1,6 @@
 /**
  * Lifted Scaml mode
- * 
+ *
  * Author: Tony Kay <tony.kay@gmail.com>
  */
 package net.liftmodules.scalate
@@ -17,24 +17,25 @@ import net.liftweb.common.Empty
 import java.io.StringReader
 import java.io.InputStreamReader
 import java.io.ByteArrayInputStream
+import net.liftweb.util.NoCache
 
 /**
  * LiftedScaml template mode.
- * 
+ *
  * This mode allows you to write files in scaml which are then pre-processed into Lift templates.
- * 
+ *
  * **IMPORTANT**: In development mode, the scaml will be run every time; however, in production mode
  * the scaml will only every be processed once, so it must be side-effect free or it will not behave
  * as expected.
- * 
+ *
  * TODO: Verify proper template caching.
  */
 object LiftedScaml {
   /**
    * Initialize Lifted Scaml mode. Requires an extra suffix on files as a marker.
-   * 
+   *
    * This installs Scalate as a template resolver into the regular Lift pipeline.
-   * 
+   *
    * Templates must be named xxx.l.scaml
    */
   def init = {
@@ -44,10 +45,10 @@ object LiftedScaml {
   }
 }
 
-class ScamlTemplateLoader(suffix : String) extends Loggable {
+class ScamlTemplateLoader(suffix: String) extends Loggable {
   val renderer = new LiftTemplateEngine
 
-  protected def createUri(path: List[String]): String = path.mkString("/") + "." + suffix 
+  protected def createUri(path: List[String]): String = path.mkString("/") + "." + suffix
 
   protected def canLoad(v: String): Boolean = v.endsWith(".l.scaml") && renderer.canLoad(v)
 
@@ -55,13 +56,23 @@ class ScamlTemplateLoader(suffix : String) extends Loggable {
 
   def scalateTemplateLoader: PartialFunction[(Locale, List[String]), Box[NodeSeq]] = {
     // TODO: Add locale suffix support
-    case (l, path) if (canRender(path)) => {
+    case (locale, path) if (canRender(path)) => {
       val uri: String = createUri(path)
-      logger.debug("Loading template at " + uri)
-      val rawTemplate = renderer.layout(uri)
-      val is = new ByteArrayInputStream(rawTemplate.getBytes("UTF-8"));
-      val parserFunction: InputStream => Box[NodeSeq] = S.htmlProperties.htmlParser
-      parserFunction(is)
+      val lrCache = LiftRules.templateCache
+      val cache = if (lrCache.isDefined) lrCache.openOrThrowException("Internal Cache Error") else NoCache
+      val key = (locale, path)
+      val cachedTemplate = cache.get(key)
+      if (cachedTemplate.isDefined) {
+        cachedTemplate
+      } else {
+        logger.debug("Loading template at " + uri)
+        val rawTemplate = renderer.layout(uri)
+        val is = new ByteArrayInputStream(rawTemplate.getBytes("UTF-8"));
+        val parserFunction: InputStream => Box[NodeSeq] = S.htmlProperties.htmlParser
+        val template = parserFunction(is)
+        template.map(t => cache(key) = t)
+        template
+      }
     }
   }
 }
